3aba08ae07e19c84305209a828974e881fe92b5c
Neuxbane <neuxbane@gmail.com>
2024-05-19 09:21:05 +0700

    upload engine

---
+++ config.js
@@ -0,0 +1,3 @@
+module.exports = {
+	"APIKeys": ["YOUR-GEMINI-API-KEY"]
+}
---
+++ test.js
@@ -0,0 +1,15 @@
+const { GeminiHandler } = require("./lib/engine");
+const config = require('./config');
+const functions = require('./functions');
+
+
+(async()=>{
+	const handler = new GeminiHandler(config.APIKeys);
+	let dt = {part: "Hello, what time is it in Yogyakarta?", functions:functions};
+	while(true){
+		const response = (await handler.generate(dt));
+		console.log(response)
+		if(response.next) dt = response.next;
+		else break;
+	}
+})();
